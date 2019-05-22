edd8ee35b3fb70ac767b232899d3984bf684d7dc
fernandesnetobr <fernandesneto.br@gmail.com>
2019-05-22 19:31:35 -0300

    comit3

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,35 @@ class _State extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
         actions: <Widget>[
-          IconButton( icon: Icon(Icons.refresh),
-
-          )
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.account_box, size: 130.0, color: Colors.deepOrange),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (KG)",
+                  labelStyle: TextStyle(color: Colors.black, fontSize: 20.0)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.deepOrange, fontSize: 20)
+          ),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (KG)",
+                  labelStyle: TextStyle(color: Colors.black, fontSize: 20.0)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.deepOrange, fontSize: 20)
+          ),
         ],
       ),
     );
